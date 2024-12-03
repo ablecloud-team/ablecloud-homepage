707aa8b041fa6684de0342e97296ea0e212f0aba
Kim jonghun <developerkimjh@gmail.com>
2024-12-03 11:12:49 +0900

    FEAT: add MainPageHeader

---
+++ src/templates/common/MainPageHeader.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui';
+
+import { HeaderData } from '@/types/header';
+
+export function MainPageHeader({ headerData }: { headerData: HeaderData }) {
+  const { title, description, bgClassName, heightClassName, button } = headerData;
+
+  return (
+    <div
+      className={`!flex items-center justify-center px-4 bg-cover bg-no-repeat bg-center text-white text-center break-all mt-[62px] md:mt-0 ${bgClassName} ${heightClassName}`}>
+      <div className='flex flex-col items-center'>
+        <div className='text-[28px] leading-[36px] font-bold lg:text-[48px] lg:leading-[57.6px]'>
+          {title.mainText}
+          {title.subText && (
+            <span className='text-[24px] leading-[28.8px] lg:text-[36px] lg:leading-[43.2px]'>
+              {` ${title.subText}`}
+            </span>
+          )}
+        </div>
+        <div className='mt-[12px] font-medium text-[16px] leading-[24px] lg:text-[20px] lg:leading-[30px] '>
+          {description}
+        </div>
+        {button && (
+          <Button text={button.text} href={button.href} className={`mt-6 ${button.className}`} />
+        )}
+      </div>
+    </div>
+  );
+}
