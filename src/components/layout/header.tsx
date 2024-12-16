e6fc3a39cfc57e0bd2d3cd98711cff9ba7d5202b
Kim jonghun <developerkimjh@gmail.com>
2024-12-16 09:53:34 +0900

    FEAT: add header submenu

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,13 +9,44 @@ import Close from '@/public/icons/common/close.svg';
 import Hamburger from '@/public/icons/common/hamburger.svg';
 
 const menuItems = [
-  { title: '제품', href: '/products/ablestack' },
+  {
+    title: '제품',
+    href: '/products/ablestack',
+    subMenu: [
+      { title: 'ABLESTACK VM', href: '/products/ablestack' },
+      { title: 'ABLESTACK HCI', href: '/products/ablestack' },
+    ],
+  },
   { title: '적용분야', href: '/applications' },
   { title: '성공사례', href: '/' },
-  { title: '파트너', href: '/' },
-  { title: '리소스', href: '/resource/blog' },
+  {
+    title: '파트너',
+    href: '/',
+    subMenu: [
+      { title: '프로그램 소개', href: '/' },
+      { title: '파트너 현황', href: '/' },
+    ],
+  },
+  {
+    title: '리소스',
+    href: '/resource/blog',
+    subMenu: [
+      { title: '소식', href: '/resource/blog' },
+      { title: '동영상', href: '/resource/video' },
+      { title: '백서/ebook', href: '/resource/ebook' },
+    ],
+  },
   { title: '제품문의', href: '/' },
-  { title: '회사소개', href: '/' },
+  {
+    title: '회사소개',
+    href: '/',
+    subMenu: [
+      { title: '회사소개 및 비전', href: '/' },
+      { title: '연혁', href: '/' },
+      { title: '인증 및 특허', href: '/' },
+      { title: '찾아오시는 길', href: '/' },
+    ],
+  },
 ];
 
 export function Header() {
@@ -57,17 +88,34 @@ export function Header() {
     <div
       className={`flex flex-col z-50 w-full items-center ${isMenuOpen ? 'bg-white text-black' : 'text-white bg-[#202020]'} md:fixed ${bgStyle}`}>
       <div
-        className={`flex max-w-[1440px] w-full items-center ${isMenuOpen ? 'justify-end' : 'justify-between'} px-[19px] h-[60px]`}>
+        className={`flex max-w-[1440px] w-full items-center ${isMenuOpen ? 'justify-end' : 'justify-between'} pl-[19px] pr-[19px] md:pr-[55px] xl:pr-[19px] h-[60px]`}>
         {!isMenuOpen && (
           <Link href={'/'}>
             <Ablestack />
           </Link>
         )}
-        <div className='gap-5 px-[10px] font-bold hidden md:flex'>
+        <div className='gap-5 px-[10px] font-bold hidden cursor-pointer md:flex'>
           {menuItems.map((v, index) => (
-            <Link href={v.href} key={index}>
-              {v.title}
-            </Link>
+            <div className='relative group' key={index}>
+              <Link
+                href={v.href}
+                className={`${scrollPosition === 'bottom' ? 'hover:text-[#202020]' : 'hover:text-[#f5f5f5]'} py-2 block`}>
+                {v.title}
+              </Link>
+              {v.subMenu && (
+                <div className='absolute p-[14px] rounded-lg left-1/2 -translate-x-1/2 w-[170px] bg-black text-white opacity-0 transform translate-y-4 transition-all duration-200 group-hover:opacity-100 group-hover:translate-y-0'>
+                  {v.subMenu?.map((subItem, subIndex) => (
+                    <div key={subIndex}>
+                      <Link
+                        href={subItem.href}
+                        className='block py-2 text-[#ddd] text-[14px] duration-100 hover:text-white rounded'>
+                        {subItem.title}
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           ))}
         </div>
         <div className='cursor-pointer h-[30px] items-center select-none border px-[17px] text-[13px] rounded-md hidden lg:flex'>
