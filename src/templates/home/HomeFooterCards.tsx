89afff94be4c9956f1badb665f0b61cbdddc4c91
Kim jonghun <developerkimjh@gmail.com>
2025-01-08 10:56:25 +0900

    FIX: fix style

--- src/templates/home/HomeFooterCards.tsx
+++ src/templates/home/HomeFooterCards.tsx
@@ -13,9 +13,11 @@ export function HomeFooterCards({ cardsData }: HomeFooterCardsProps) {
         return (
           <div
             key={idx}
-            className={`flex flex-col gap-[9px] ${cardData.bgColor} min-w-[342px] py-5 px-6 rounded-[12px] text-white`}>
+            className={`flex flex-col gap-[9px] ${cardData.bgColor} min-w-[314px] md:min-w-[342px] py-5 px-6 rounded-[12px] text-white`}>
             <div className='font-bold text-[15px] leading-[22px]'>{cardData.category}</div>
-            <div className='font-bold text-[20px] leading-[30px]'>{cardData.text}</div>
+            <div className='font-bold text-[18px] md:text-[20px] leading-[30px]'>
+              {cardData.text}
+            </div>
             <div className='flex mb-[11px]'>
               <Button buttonData={cardData.buttonData} />
             </div>
