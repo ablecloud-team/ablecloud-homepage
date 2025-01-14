0f25093c014120c6607f26546eac7c843e04a22a
Kim jonghun <developerkimjh@gmail.com>
2025-01-14 10:30:31 +0900

    FEAT: add sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
-import { Footer, Header } from '@/components/layout';
+import { Metadata } from 'next';
 
 import './globals.css';
+
+export const metadata: Metadata = {
+  verification: {
+    google: '6o5Lcww7U6jkboi0ZpvyonRKDh_L8pEpUUtJFi0exOE',
+    other: {
+      'naver-site-verification': '9dbc76f924814a3fc2e83542a1705ef71cc123d8',
+    },
+  },
+};
 
 export default function Layout({
   children,
