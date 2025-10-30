19160a21ac4973f88e155258bb872459785a599b
Jeon-man <jhmjhm0106@naver.com>
2025-10-30 10:32:43 +0900

    style(MainPageHeader): adjust typography spacing and line heights

--- src/templates/common/MainPageHeader.tsx
+++ src/templates/common/MainPageHeader.tsx
@@ -9,18 +9,18 @@ export function MainPageHeader({ headerData }: { headerData: HeaderData }) {
     <div
       className={`flex items-center justify-center px-4 bg-cover bg-no-repeat bg-center text-white text-center break-keep md:pt-[60px] ${bgClassName} ${heightClassName}`}>
       <div className='flex flex-col items-center'>
-        <div className='text-[28px] leading-[36px] font-bold lg:text-[48px] lg:leading-[57.6px]'>
+        <div className='text-[28px] leading-tight font-bold lg:text-[48px] lg:leading-[48px]'>
           {title.mainText}
           {title.subText && (
-            <span className='text-[24px] leading-[28.8px] lg:text-[36px] lg:leading-[43.2px]'>
+            <span className='text-[24px] leading-[28.8px] lg:text-[36px] lg:leading-[40px]'>
               {` ${title.subText}`}
             </span>
           )}
         </div>
-        <div className='mt-[12px] font-medium text-[16px] leading-[24px] lg:text-[18px] lg:leading-[30px] '>
+        <div className='mt-[12px] font-medium text-[16px] leading-[20px] lg:text-[18px] lg:leading-[24px] '>
           {description}
         </div>
-        {buttonData && <Button className={`mt-6 ${buttonData.className ?? ''}`} {...buttonData} />}
+        {buttonData && <Button className={`mt-4 ${buttonData.className ?? ''}`} {...buttonData} />}
       </div>
     </div>
   );
