ce0eb7b8c5488a8fced58f6160e4bc1f04d85bbf
Jeon-man <jhmjhm0106@naver.com>
2026-06-24 15:25:35 +0900

    UPDATE: modify InterviewList layout for improved responsiveness and adjust image dimensions for better visual alignment

--- src/templates/interview/InterviewList.tsx
+++ src/templates/interview/InterviewList.tsx
@@ -10,15 +10,15 @@ export function InterViewList({ interviewList }: { interviewList: InterviewHisto
       <div className='font-bold text-center text-[24px] leading-[32px] md:text-[30px] md:leading-[43px] pb-5'>
         {t('interview.title')}
       </div>
-      <div className='flex flex-col gap-8 max-w-[1260px] w-full'>
+      <div className='flex w-full max-w-[1298px] flex-col gap-8'>
         {interviewList.map(({ category, data }, catIdx) => (
           <section key={catIdx} className='flex flex-col gap-4'>
             <h2 className='text-lg font-semibold text-[#222]'>{category}</h2>
-            <div className='grid gap-x-3 gap-y-2 grid-cols-[repeat(auto-fit,_minmax(200px,1fr))] justify-center'>
+            <div className='grid grid-cols-2 gap-3 md:grid-cols-3 xl:grid-cols-6'>
               {data.map((item, idx) => (
                 <div
                   key={idx}
-                  className='flex justify-center flex-col gap-2 min-h-[150px] items-center px-6 py-4 bg-white rounded-[10px] border border-[#EBEEF5]'>
+                  className='flex h-[140px] items-center justify-center rounded-[10px] border border-[#EBEEF5] bg-white px-6 py-4'>
                   {item.img}
                 </div>
               ))}
